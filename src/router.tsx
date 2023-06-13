27663d3995631f3d58ac10c33ed6522af89508ac
PavelMosolov79 <78637178+PavelMosolov79@users.noreply.github.com>
2023-06-13 22:35:18 +0700

    Create page other

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { Other } from "./pages/Other";
 
 export const Router = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="/other" element={<Other/>}/>
     </Routes>
   );
 };
