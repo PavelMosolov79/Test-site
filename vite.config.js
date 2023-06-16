ce0dccb3a3699c3820f0832a4f1c549d15e5c08d
PavelMosolov79 <78637178+PavelMosolov79@users.noreply.github.com>
2023-06-17 00:37:08 +0700

    Vite ssr + express + node js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
 
-export default {
-  plugins: [react(), ssr()]
-}
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    minify: false,
+  },
+})
