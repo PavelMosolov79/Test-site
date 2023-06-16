4a3c9b43482c5dde455e1959dee1f7fe46f5f701
PavelMosolov79 <78637178+PavelMosolov79@users.noreply.github.com>
2023-06-16 18:59:34 +0700

    dist ssr

---
+++ dist/client/assets/entries/pages_about_index.page.e47f2653.js
@@ -0,0 +1,1 @@
+import{a as e,F as r,j as n}from"../chunks/chunk-ad3e06ed.js";function i(){return e(r,{children:[n("h1",{children:"About"}),e("p",{children:["Example of using ",n("code",{children:"vite-plugin-ssr"}),"."]})]})}export{i as Page};
